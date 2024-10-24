6f93e689c3e36fb1b51b8cb5799cd9516a0e0de0
Ballet-ux <mpanoap@gmail.com>
2024-10-24 16:13:15 +0200

    Delete question3.py

--- question3.py
+++
@@ -1,8 +0,0 @@
-def sum_odd_even (numbers):
-    even_sum = sum(i for i in numbers if i % 2 ==0)
-    odd_sum = sum(i for i in numbers if i % 2 !=0)
-    return even_sum,odd_sum
-
-numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-result = sum_odd_even(numbers)
-print(result)
