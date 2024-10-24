d99831fa41ab4f03c630f6d15c42528eec869832
Ballet-ux <mpanoap@gmail.com>
2024-10-24 15:34:20 +0200

    Initial commit

---
+++ question 1.py
@@ -0,0 +1,7 @@
+def string_lengths(strings):
+    return {string: len(string) for string in strings}
+
+# Example usage:
+my_strings = ["apple", "banana", "cherry"]
+result = string_lengths(my_strings)
+print(result)
